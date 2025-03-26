355f988256200ecf3fc63e0664248f07893b6e5f
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-03-26 10:56:03 +0100

    Started implementation of POST /api/v2/cases/{case_identifier}/events to create events

---
+++ source/app/blueprints/rest/v2/case_objects/events.py
@@ -0,0 +1,32 @@
+#  IRIS Source Code
+#  Copyright (C) 2024 - DFIR-IRIS
+#  contact@dfir-iris.org
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from flask import Blueprint
+
+from app.blueprints.access_controls import ac_api_requires
+from app.blueprints.rest.endpoints import response_api_created
+from app.schema.marshables import EventSchema
+
+
+case_events_blueprint = Blueprint('case_events_rest_v2', __name__, url_prefix='/<int:case_identifier>/events')
+
+
+@case_events_blueprint.post('')
+@ac_api_requires()
+def create_evidence(case_identifier):
+    return response_api_created(None)
