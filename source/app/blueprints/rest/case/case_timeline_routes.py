9076a3f5e743d6ad629a6ede060fb879ac0a441d
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-04-02 12:07:24 +0200

    Deprecated GET /case/timeline/events/{cur_id} in favor of /api/v2/cases/{case_identifier}/events/{identifier}

--- source/app/blueprints/rest/case/case_timeline_routes.py
+++ source/app/blueprints/rest/case/case_timeline_routes.py
@@ -661,6 +661,7 @@ def event_flag(cur_id, caseid):
 
 
 @case_timeline_rest_blueprint.route('/case/timeline/events/<int:cur_id>', methods=['GET'])
+@endpoint_deprecated('GET', '/api/v2/cases/{case_identifier}/events/{identifier}')
 @ac_requires_case_identifier(CaseAccessLevel.read_only, CaseAccessLevel.full_access)
 @ac_api_requires()
 def event_view(cur_id, caseid):
