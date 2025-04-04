2c1b9b25d751c5bfee9870ac10e1a1949a96b101
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-04-04 13:41:58 +0200

    Syntax formatting

--- source/app/iris_engine/utils/collab.py
+++ source/app/iris_engine/utils/collab.py
@@ -11,13 +11,10 @@ def collab_notify(case_id: int,
                   request_sid: int = None
                   ):
     room = f"case-{case_id}"
-    app.socket_io.emit('case-obj-notif',
-                       json.dumps({
-                            'object_id': object_id,
-                            'action_type': action_type,
-                            'object_type': object_type,
-                            'object_data': object_data
-                        }),
-                       room=room,
-                       to=room,
-                       skip_sid=request_sid)
+    data = json.dumps({
+        'object_id': object_id,
+        'action_type': action_type,
+        'object_type': object_type,
+        'object_data': object_data
+    })
+    app.socket_io.emit('case-obj-notif', data, room=room, to=room, skip_sid=request_sid)
