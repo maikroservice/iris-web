ef8e08cba2fd89c62138ba433e9f95d82d8d7575
Elise17 <elise17web@gmail.com>
2025-04-08 17:39:21 +0200

    [IMP] Changed copyright year for tests_rest_alerts

--- tests/tests_rest_alerts.py
+++ tests/tests_rest_alerts.py
@@ -1,5 +1,5 @@
 #  IRIS Source Code
-#  Copyright (C) 2025 - DFIR-IRIS
+#  Copyright (C) 2023 - DFIR-IRIS
 #  contact@dfir-iris.org
 #
 #  This program is free software; you can redistribute it and/or
