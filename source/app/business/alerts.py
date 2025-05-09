47d3ff6898fc6e2b34cb1d3818aaa98276b2d232
Elise17 <elise17web@gmail.com>
2025-05-09 15:39:47 +0200

    Removed alert in business update_alert

--- source/app/business/alerts.py
+++ source/app/business/alerts.py
@@ -70,7 +70,7 @@ def alerts_get(current_user, identifier) -> Alert:
     return alert
 
 
-def alerts_update(alert: Alert, updated_alert: Alert, activity_data, do_resolution_hook, do_status_hook) -> Alert:
+def alerts_update(updated_alert: Alert, activity_data, do_resolution_hook, do_status_hook) -> Alert:
 
     db.session.commit()
 
@@ -84,11 +84,11 @@ def alerts_update(alert: Alert, updated_alert: Alert, activity_data, do_resoluti
 
     if activity_data:
         activity_data_as_string = ','.join(activity_data)
-        track_activity(f'updated alert #{alert.alert_id}: {activity_data_as_string}', ctx_less=True)
+        track_activity(f'updated alert #{updated_alert.alert_id}: {activity_data_as_string}', ctx_less=True)
         add_obj_history_entry(updated_alert, f'updated alert: {activity_data_as_string}')
     else:
-        track_activity(f'updated alert #{alert.alert_id}', ctx_less=True)
+        track_activity(f'updated alert #{updated_alert.alert_id}', ctx_less=True)
         add_obj_history_entry(updated_alert, 'updated alert')
 
     db.session.commit()
-    return alert
+    return updated_alert
