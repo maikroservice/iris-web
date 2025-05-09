0863b672336d7573fee8ca7a8cd9fad72aad532a
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-05-09 08:36:28 +0200

    Created an array of constants for asset types. (separate data from control)

--- source/app/post_init.py
+++ source/app/post_init.py
@@ -82,6 +82,48 @@ from app.datamgmt.manage.manage_users_db import add_user_to_organisation
 from app.datamgmt.manage.manage_groups_db import get_group_by_name
 
 
+_ASSET_TYPES= [
+    {'asset_name': 'Account', 'asset_description': 'Generic Account',
+     'asset_icon_not_compromised': 'user.png', 'asset_icon_compromised': 'ioc_user.png'},
+    {'asset_name': 'Firewall', 'asset_description': 'Firewall',
+     'asset_icon_not_compromised': 'firewall.png', 'asset_icon_compromised': 'ioc_firewall.png'},
+    {'asset_name': 'Linux - Server', 'asset_description': 'Linux server',
+     'asset_icon_not_compromised': 'server.png', 'asset_icon_compromised': 'ioc_server.png'},
+    {'asset_name': 'Linux - Computer', 'asset_description': 'Linux computer',
+     'asset_icon_not_compromised': 'desktop.png', 'asset_icon_compromised': 'ioc_desktop.png'},
+    {'asset_name': 'Linux Account', 'asset_description': 'Linux Account',
+     'asset_icon_not_compromised': 'user.png', 'asset_icon_compromised': 'ioc_user.png'},
+    {'asset_name': 'Mac - Computer', 'asset_description': 'Mac computer',
+     'asset_icon_not_compromised': 'desktop.png', 'asset_icon_compromised': 'ioc_desktop.png'},
+    {'asset_name': 'Phone - Android', 'asset_description': 'Android Phone',
+     'asset_icon_not_compromised': 'phone.png', 'asset_icon_compromised': 'ioc_phone.png'},
+    {'asset_name': 'Phone - IOS', 'asset_description': 'Apple Phone',
+     'asset_icon_not_compromised': 'phone.png', 'asset_icon_compromised': 'ioc_phone.png'},
+    {'asset_name': 'Windows - Computer', 'asset_description': 'Standard Windows Computer',
+     'asset_icon_not_compromised': 'windows_desktop.png', 'asset_icon_compromised': 'ioc_windows_desktop.png'},
+    {'asset_name': 'Windows - Server', 'asset_description': 'Standard Windows Server',
+     'asset_icon_not_compromised': 'windows_server.png', 'asset_icon_compromised': 'ioc_windows_server.png'},
+    {'asset_name': 'Windows - DC', 'asset_description': 'Domain Controller',
+     'asset_icon_not_compromised': 'windows_server.png', 'asset_icon_compromised': 'ioc_windows_server.png'},
+    {'asset_name': 'Router', 'asset_description': 'Router', 'asset_icon_not_compromised': 'router.png', 'asset_icon_compromised': 'ioc_router.png'},
+    {'asset_name': 'Switch', 'asset_description': 'Switch', 'asset_icon_not_compromised': 'switch.png', 'asset_icon_compromised': 'ioc_switch.png'},
+    {'asset_name': 'VPN', 'asset_description': 'VPN', 'asset_icon_not_compromised': 'vpn.png', 'asset_icon_compromised': 'ioc_vpn.png'},
+    {'asset_name': 'WAF', 'asset_description': 'WAF', 'asset_icon_not_compromised': 'firewall.png', 'asset_icon_compromised': 'ioc_firewall.png'},
+    {'asset_name': 'Windows Account - Local', 'asset_description': 'Windows Account - Local',
+     'asset_icon_not_compromised': 'user.png', 'asset_icon_compromised': 'ioc_user.png'},
+    {'asset_name': 'Windows Account - Local - Admin', 'asset_description': 'Windows Account - Local - Admin',
+     'asset_icon_not_compromised': 'user.png', 'asset_icon_compromised': 'ioc_user.png'},
+    {'asset_name': 'Windows Account - AD', 'asset_description': 'Windows Account - AD',
+     'asset_icon_not_compromised': 'user.png', 'asset_icon_compromised': 'ioc_user.png'},
+    {'asset_name': 'Windows Account - AD - Admin', 'asset_description': 'Windows Account - AD - Admin',
+     'asset_icon_not_compromised': 'user.png', 'asset_icon_compromised': 'ioc_user.png'},
+    {'asset_name': 'Windows Account - AD - krbtgt', 'asset_description': 'Windows Account - AD - krbtgt',
+     'asset_icon_not_compromised': 'user.png', 'asset_icon_compromised': 'ioc_user.png'},
+    {'asset_name': 'Windows Account - AD - Service', 'asset_description': 'Windows Account - AD - krbtgt',
+     'asset_icon_not_compromised': 'user.png', 'asset_icon_compromised': 'ioc_user.png'}
+]
+
+
 def connect_to_database(host: str, port: int) -> bool:
     """Attempts to connect to a database at the specified host and port.
 
@@ -650,65 +692,8 @@ def create_safe_assets():
 
     """
     # Create new AssetsType objects for each asset type
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Account",
-                                    'asset_description': "Generic Account", 'asset_icon_not_compromised': "user.png",
-                                    'asset_icon_compromised': "ioc_user.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Firewall", 'asset_description': "Firewall",
-                                    'asset_icon_not_compromised': "firewall.png", 'asset_icon_compromised': "ioc_firewall.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Linux - Server",
-                                    'asset_description': "Linux server", 'asset_icon_not_compromised': "server.png",
-                                    'asset_icon_compromised': "ioc_server.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Linux - Computer",
-                                    'asset_description': "Linux computer", 'asset_icon_not_compromised': "desktop.png",
-                                    'asset_icon_compromised': "ioc_desktop.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Linux Account",
-                                    'asset_description': "Linux Account", 'asset_icon_not_compromised': "user.png",
-                                    'asset_icon_compromised': "ioc_user.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Mac - Computer",
-                                    'asset_description': "Mac computer", 'asset_icon_not_compromised': "desktop.png",
-                                    'asset_icon_compromised': "ioc_desktop.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Phone - Android",
-                                    'asset_description': "Android Phone", 'asset_icon_not_compromised': "phone.png",
-                                    'asset_icon_compromised': "ioc_phone.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Phone - IOS",
-                                    'asset_description': "Apple Phone", 'asset_icon_not_compromised': "phone.png",
-                                    'asset_icon_compromised': "ioc_phone.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Windows - Computer",
-                                    'asset_description': "Standard Windows Computer",
-                                    'asset_icon_not_compromised': "windows_desktop.png",
-                                    'asset_icon_compromised': "ioc_windows_desktop.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Windows - Server",
-                                    'asset_description': "Standard Windows Server", 'asset_icon_not_compromised': "windows_server.png",
-                                    'asset_icon_compromised': "ioc_windows_server.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Windows - DC",
-                                    'asset_description': "Domain Controller", 'asset_icon_not_compromised': "windows_server.png",
-                                    'asset_icon_compromised': "ioc_windows_server.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Router", 'asset_description': "Router",
-                                    'asset_icon_not_compromised': "router.png", 'asset_icon_compromised': "ioc_router.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Switch", 'asset_description': "Switch",
-                                    'asset_icon_not_compromised': "switch.png", 'asset_icon_compromised': "ioc_switch.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "VPN", 'asset_description': "VPN",
-                                    'asset_icon_not_compromised': "vpn.png", 'asset_icon_compromised': "ioc_vpn.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "WAF", 'asset_description': "WAF",
-                                    'asset_icon_not_compromised': "firewall.png", 'asset_icon_compromised': "ioc_firewall.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Windows Account - Local",
-                                    'asset_description': "Windows Account - Local", 'asset_icon_not_compromised': "user.png",
-                                    'asset_icon_compromised': "ioc_user.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Windows Account - Local - Admin",
-                                    'asset_description': "Windows Account - Local - Admin", 'asset_icon_not_compromised': "user.png",
-                                    'asset_icon_compromised': "ioc_user.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Windows Account - AD",
-                                    'asset_description': "Windows Account - AD", 'asset_icon_not_compromised': "user.png",
-                                    'asset_icon_compromised': "ioc_user.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Windows Account - AD - Admin",
-                                    'asset_description': "Windows Account - AD - Admin", 'asset_icon_not_compromised': "user.png",
-                                    'asset_icon_compromised': "ioc_user.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Windows Account - AD - krbtgt",
-                                    'asset_description': "Windows Account - AD - krbtgt", 'asset_icon_not_compromised': "user.png",
-                                    'asset_icon_compromised': "ioc_user.png"}))
-    create_asset_type_if_not_exists(db.session, AssetsType(**{'asset_name': "Windows Account - AD - Service",
-                                    'asset_description': "Windows Account - AD - krbtgt", 'asset_icon_not_compromised': "user.png",
-                                    'asset_icon_compromised': "ioc_user.png"}))
+    for asse_type in _ASSET_TYPES:
+        create_asset_type_if_not_exists(db.session, AssetsType(**asse_type))
 
 
 def create_safe_client():
