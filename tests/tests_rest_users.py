eaba2998b60654a00c66f743bcd751d3ef80b65d
Elise17 <elise17web@gmail.com>
2025-05-23 10:25:20 +0200

    Added test_create_user_should_return_400_when_user_name_field_is_missing

--- tests/tests_rest_users.py
+++ tests/tests_rest_users.py
@@ -61,3 +61,14 @@ class TestsRestUsers(TestCase):
         }
         response = self._subject.create('api/v2/manage/users', body).json()
         self.assertEqual(user_name, response['user_name'])
+
+    def test_create_user_should_return_400_when_user_name_field_is_missing(self):
+        body = {
+            'user_login': 'new_user_login',
+            'user_email': 'new_user_email',
+            'user_password': 'NEW_user_password_17_@',
+            'user_is_service_account': True,
+            'user_isadmin': True,
+        }
+        response = self._subject.create('api/v2/manage/users', body)
+        self.assertEqual(400, response.status_code)
