e2ac6c842a0c37057cbd94d171ecb3e4c335e2f7
Elise17 <elise17web@gmail.com>
2025-05-23 15:34:01 +0200

    Fixed bug

--- source/app/blueprints/rest/v2/manage_routes/groups.py
+++ source/app/blueprints/rest/v2/manage_routes/groups.py
@@ -75,6 +75,9 @@ class Groups:
             result = self._schema.dump(group)
             return response_api_success(result)
 
+        except ValidationError as e:
+            return response_api_error('Data error', data=e.messages)
+
         except ObjectNotFoundError:
             return response_api_not_found()
 
