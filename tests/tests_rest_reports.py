c61de19a0b8b151a7a32f4c57be5f57f33a76dbf
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-06-11 14:07:11 +0200

    Added test to check report generation should not fail

---
+++ tests/tests_rest_reports.py
@@ -0,0 +1,45 @@
+#  IRIS Source Code
+#  Copyright (C) 2023 - DFIR-IRIS
+#  contact@dfir-iris.org
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from unittest import TestCase
+
+from iris import Iris
+
+
+class TestsRestReports(TestCase):
+
+    def setUp(self) -> None:
+        self._subject = Iris()
+
+    def tearDown(self):
+        self._subject.clear_database()
+        response = self._subject.get('/manage/templates/list').json()
+        for report_template in response['data']:
+            identifier = report_template['id']
+            self._subject.create(f'/manage/templates/delete/{identifier}', {})
+
+    def test_generate_report__in_safe_mode_should_return_200(self):
+        data = {'report_name': 'name', 'report_type': 1, 'report_language': 1, 'report_description': 'description',
+                'report_name_format': 'report_name_format'}
+        response = self._subject.post_multipart_encoded_file('/manage/templates/add', data,
+                                                          'data/report_templates/empty_report_template.docx').json()
+        report_identifier = response['data']['report_id']
+        case_identifier = self._subject.create_dummy_case()
+        response = self._subject.get(f'/case/report/generate-investigation/{report_identifier}',
+                                     {'cid': case_identifier, 'safe': True})
+        self.assertEqual(200, response.status_code)
