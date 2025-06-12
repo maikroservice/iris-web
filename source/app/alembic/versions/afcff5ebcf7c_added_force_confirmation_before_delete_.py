ffebd5aca78ae160b471914d64e576dd3e6c6f23
Derick Diaz <derickdiaz123@outlook.com>
2025-06-11 19:35:13 -0500

    Updated migration script

---
+++ source/app/alembic/versions/afcff5ebcf7c_added_force_confirmation_before_delete_.py
@@ -0,0 +1,24 @@
+"""Added force_confirmation_before_delete to ServerSettings
+
+Revision ID: afcff5ebcf7c
+Revises: d5a720d1b99b
+Create Date: 2025-06-12 00:33:12.873850
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'afcff5ebcf7c'
+down_revision = 'd5a720d1b99b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
