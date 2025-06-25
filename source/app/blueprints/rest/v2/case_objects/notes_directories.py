a8475b04680ea993d9314faf1e7101b62003f9fa
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-06-25 14:57:26 +0200

    Started code of POST /api/v2/cases/{identifier}/notes-directories to create notes directories

---
+++ source/app/blueprints/rest/v2/case_objects/notes_directories.py
@@ -0,0 +1,38 @@
+#  IRIS Source Code
+#  Copyright (C) 2024 - DFIR-IRIS
+#  contact@dfir-iris.org
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from flask import Blueprint
+
+from app.blueprints.access_controls import ac_api_requires
+from app.blueprints.rest.endpoints import response_api_created
+
+
+class NotesDirectories:
+
+    def create(self, case_identifier):
+        return response_api_created(None)
+
+
+notes_directories = NotesDirectories()
+case_notes_directories_blueprint = Blueprint('case_notes_directoriesrest_v2', __name__, url_prefix='/<int:case_identifier>/notes-directories')
+
+
+@case_notes_directories_blueprint.post('')
+@ac_api_requires()
+def create_event(case_identifier):
+    return notes_directories.create(case_identifier)
