b1bd3d30a5bb4ecba18593e65ea71981cb59fb10
Elise17 <elise17web@gmail.com>
2025-07-01 14:31:01 +0200

    Removed cases_access from class User

--- source/app/models/authorization.py
+++ source/app/models/authorization.py
@@ -222,7 +222,6 @@ class User(UserMixin, db.Model):
     groups = relationship('Group', secondary='user_group', viewonly=True)
     permissions = relationship('Group', secondary='user_group', viewonly=True)
     customers = relationship('Client', secondary='user_client', viewonly=True)
-    cases_access = relationship('UserCaseAccess', back_populates='user')
 
     def __init__(self, user: str, name: str, email: str, password: str, active: bool,
                  external_id: str = None, is_service_account: bool = False, mfa_secret: str = None,
