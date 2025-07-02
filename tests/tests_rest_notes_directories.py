0ffa3564b686046887c551cdbd9ca0daf66dc22a
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-07-02 10:53:48 +0200

    PUT /api/v2/cases/{case_identifier}/notes-directories/{identifier} returns 403 when user has no access to case

--- tests/tests_rest_notes_directories.py
+++ tests/tests_rest_notes_directories.py
@@ -144,3 +144,12 @@ class TestsRestNotesDirectories(TestCase):
         response = self._subject.update(f'/api/v2/cases/{case_identifier}/notes-directories/{_IDENTIFIER_FOR_NONEXISTENT_OBJECT}', {})
         self.assertEqual(404, response.status_code)
 
+    def test_update_note_directory_should_return_403_when_user_has_no_access_to_case(self):
+        case_identifier = self._subject.create_dummy_case()
+        body = {'name': 'directory_name'}
+        response = self._subject.create(f'/api/v2/cases/{case_identifier}/notes-directories', body).json()
+        identifier = response['id']
+
+        user = self._subject.create_dummy_user()
+        response = user.update(f'/api/v2/cases/{case_identifier}/notes-directories/{identifier}', {})
+        self.assertEqual(403, response.status_code)
