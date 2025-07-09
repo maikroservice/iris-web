c473c8a00851e86dd21565ef1fa38d0beace4e89
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-07-09 11:09:45 +0200

    Introduced class for assets operations

--- source/app/blueprints/rest/v2/case_objects/assets.py
+++ source/app/blueprints/rest/v2/case_objects/assets.py
@@ -41,6 +41,103 @@ from app.iris_engine.access_control.utils import ac_fast_check_current_user_has_
 from app.models.authorization import CaseAccessLevel
 from app.schema.marshables import CaseAssetsSchema
 
+
+class AssetsOperations:
+
+    def __init__(self):
+        self._schema = CaseAssetsSchema()
+
+    @staticmethod
+    def _get_asset_in_case(identifier, case_identifier):
+        asset = assets_get(identifier)
+        if asset.case_id != case_identifier:
+            raise ObjectNotFoundError
+        return asset
+
+    def list(self, case_identifier):
+        try:
+            if not ac_fast_check_current_user_has_case_access(case_identifier,
+                                                              [CaseAccessLevel.read_only, CaseAccessLevel.full_access]):
+                return ac_api_return_access_denied(caseid=case_identifier)
+
+            pagination_parameters = parse_pagination_parameters(request)
+            fields = parse_fields_parameters(request)
+
+            assets = assets_filter(case_identifier, pagination_parameters, request.args.to_dict())
+
+            if fields:
+                asset_schema = CaseAssetsSchema(only=fields)
+            else:
+                asset_schema = self._schema
+
+            return response_api_paginated(asset_schema, assets)
+
+        except ObjectNotFoundError:
+            return response_api_not_found()
+        except BusinessProcessingError as e:
+            return response_api_error(e.get_message())
+
+    def create(self, case_identifier):
+        if not cases_exists(case_identifier):
+            return response_api_not_found()
+        if not ac_fast_check_current_user_has_case_access(case_identifier, [CaseAccessLevel.full_access]):
+            return ac_api_return_access_denied(caseid=case_identifier)
+
+        try:
+            _, asset = assets_create(case_identifier, request.get_json())
+            return response_api_created(self._schema.dump(asset))
+        except BusinessProcessingError as e:
+            return response_api_error(e.get_message(), data=e.get_data())
+
+    def get(self, case_identifier, identifier):
+        try:
+            asset = self._get_asset_in_case(identifier, case_identifier)
+
+            if not ac_fast_check_current_user_has_case_access(asset.case_id,
+                                                              [CaseAccessLevel.read_only, CaseAccessLevel.full_access]):
+                return ac_api_return_access_denied(caseid=asset.case_id)
+
+            return response_api_success(self._schema.dump(asset))
+        except ObjectNotFoundError:
+            return response_api_not_found()
+        except BusinessProcessingError as e:
+            return response_api_error(e.get_message())
+
+    def update(self,case_identifier, identifier):
+        try:
+            asset = self._get_asset_in_case(identifier, case_identifier)
+
+            if not ac_fast_check_current_user_has_case_access(asset.case_id, [CaseAccessLevel.full_access]):
+                return ac_api_return_access_denied(caseid=asset.case_id)
+
+            asset = assets_update(asset, request.get_json())
+
+            result = self._schema.dump(asset)
+            return response_api_success(result)
+
+        except ObjectNotFoundError:
+            return response_api_not_found()
+
+        except BusinessProcessingError as e:
+            return response_api_error(e.get_message(), data=e.get_data())
+
+    def delete(self, case_identifier, identifier):
+        try:
+            asset = self._get_asset_in_case(identifier, case_identifier)
+
+            # perform authz check
+            if not ac_fast_check_current_user_has_case_access(asset.case_id, [CaseAccessLevel.full_access]):
+                return ac_api_return_access_denied(caseid=asset.case_id)
+
+            assets_delete(asset)
+            return response_api_deleted()
+        except ObjectNotFoundError:
+            return response_api_not_found()
+        except BusinessProcessingError as e:
+            return response_api_error(e.get_message())
+
+
+assets_operations = AssetsOperations()
 case_assets_blueprint = Blueprint('case_assets',
                                   __name__,
                                   url_prefix='/<int:case_identifier>/assets')
@@ -49,109 +146,28 @@ case_assets_blueprint = Blueprint('case_assets',
 @case_assets_blueprint.get('')
 @ac_api_requires()
 def case_list_assets(case_identifier):
-
-    try:
-        if not ac_fast_check_current_user_has_case_access(case_identifier, [CaseAccessLevel.read_only, CaseAccessLevel.full_access]):
-            return ac_api_return_access_denied(caseid=case_identifier)
-
-        pagination_parameters = parse_pagination_parameters(request)
-        fields = parse_fields_parameters(request)
-
-        assets = assets_filter(case_identifier, pagination_parameters, request.args.to_dict())
-
-        if fields:
-            asset_schema = CaseAssetsSchema(only=fields)
-        else:
-            asset_schema = CaseAssetsSchema()
-
-        return response_api_paginated(asset_schema, assets)
-
-    except ObjectNotFoundError:
-        return response_api_not_found()
-    except BusinessProcessingError as e:
-        return response_api_error(e.get_message())
+    return assets_operations.list(case_identifier)
 
 
 @case_assets_blueprint.post('')
 @ac_api_requires()
 def add_asset(case_identifier):
-
-    if not cases_exists(case_identifier):
-        return response_api_not_found()
-    if not ac_fast_check_current_user_has_case_access(case_identifier, [CaseAccessLevel.full_access]):
-        return ac_api_return_access_denied(caseid=case_identifier)
-
-    asset_schema = CaseAssetsSchema()
-    try:
-        _, asset = assets_create(case_identifier, request.get_json())
-        return response_api_created(asset_schema.dump(asset))
-    except BusinessProcessingError as e:
-        return response_api_error(e.get_message(), data=e.get_data())
+    return assets_operations.create(case_identifier)
 
 
 @case_assets_blueprint.get('/<int:identifier>')
 @ac_api_requires()
 def get_asset(case_identifier, identifier):
-
-    asset_schema = CaseAssetsSchema()
-
-    try:
-        asset = assets_get(identifier)
-        _check_asset_and_case_identifier_match(asset, case_identifier)
-
-        if not ac_fast_check_current_user_has_case_access(asset.case_id, [CaseAccessLevel.read_only, CaseAccessLevel.full_access]):
-            return ac_api_return_access_denied(caseid=asset.case_id)
-
-        return response_api_success(asset_schema.dump(asset))
-    except ObjectNotFoundError:
-        return response_api_not_found()
-    except BusinessProcessingError as e:
-        return response_api_error(e.get_message())
+    return assets_operations.get(case_identifier, identifier)
 
 
 @case_assets_blueprint.put('/<int:identifier>')
 @ac_api_requires()
 def update_asset(case_identifier, identifier):
-    try:
-        asset = assets_get(identifier)
-        _check_asset_and_case_identifier_match(asset, case_identifier)
-
-        if not ac_fast_check_current_user_has_case_access(asset.case_id, [CaseAccessLevel.full_access]):
-            return ac_api_return_access_denied(caseid=asset.case_id)
-
-        asset = assets_update(asset, request.get_json())
-
-        asset_schema = CaseAssetsSchema()
-        result = asset_schema.dump(asset)
-        return response_api_success(result)
-
-    except ObjectNotFoundError:
-        return response_api_not_found()
-
-    except BusinessProcessingError as e:
-        return response_api_error(e.get_message(), data=e.get_data())
+    return assets_operations.update(case_identifier, identifier)
 
 
 @case_assets_blueprint.delete('/<int:identifier>')
 @ac_api_requires()
 def delete_asset(case_identifier, identifier):
-
-    try:
-        asset = assets_get(identifier)
-        _check_asset_and_case_identifier_match(asset, case_identifier)
-
-        # perform authz check
-        if not ac_fast_check_current_user_has_case_access(asset.case_id, [CaseAccessLevel.full_access]):
-            return ac_api_return_access_denied(caseid=asset.case_id)
-
-        assets_delete(asset)
-        return response_api_deleted()
-    except ObjectNotFoundError:
-        return response_api_not_found()
-    except BusinessProcessingError as e:
-        return response_api_error(e.get_message())
-
-
-def _check_asset_and_case_identifier_match(asset, case_identifier):
-    if asset.case_id != case_identifier:
-        raise ObjectNotFoundError
+    return assets_operations.delete(case_identifier, identifier)
