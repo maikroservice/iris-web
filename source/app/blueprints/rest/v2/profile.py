24b165ca5d1a0cd17bda42cbdafa67a1e387f951
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-07-11 14:40:08 +0200

    Started implementation of PUT /api/v2/me

---
+++ source/app/blueprints/rest/v2/profile.py
@@ -0,0 +1,37 @@
+#  IRIS Source Code
+#  Copyright (C) 2024 - DFIR-IRIS
+#  contact@dfir-iris.org
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from flask import Blueprint
+
+from app.blueprints.rest.endpoints import response_api_success
+from app.blueprints.access_controls import ac_api_requires
+
+
+class ProfileOperations:
+
+    def update(self):
+        return response_api_success(None)
+
+profile_operations = ProfileOperations()
+profile_blueprint = Blueprint('profile_rest_v2', __name__, url_prefix='/me')
+
+
+@profile_blueprint.put('')
+@ac_api_requires()
+def update_profile():
+    return profile_operations.update()
---
+++ tests/tests_rest_profile.py
@@ -0,0 +1,33 @@
+#  IRIS Source Code
+#  Copyright (C) 2023 - DFIR-IRIS
+#  contact@dfir-iris.org
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 3 of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from unittest import TestCase
+from iris import Iris
+
+
+class TestsRestProfile(TestCase):
+
+    def setUp(self) -> None:
+        self._subject = Iris()
+
+    def tearDown(self):
+        self._subject.clear_database()
+
+    def test_update_me_should_return_200(self):
+        response = self._subject.update(f'/api/v2/me', {})
+        self.assertEqual(200, response.status_code)
