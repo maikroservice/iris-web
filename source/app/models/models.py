acd242b87a07c0479ef8dac8e5353aa48c61bd95
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-10-08 10:52:26 +0200

    Early return

--- source/app/models/models.py
+++ source/app/models/models.py
@@ -109,11 +109,11 @@ def get_or_create(session, model, **kwargs):
     instance = session.query(model).filter_by(**kwargs).first()
     if instance:
         return instance
-    else:
-        instance = model(**kwargs)
-        session.add(instance)
-        session.commit()
-        return instance
+
+    instance = model(**kwargs)
+    session.add(instance)
+    session.commit()
+    return instance
 
 
 class Client(db.Model):
