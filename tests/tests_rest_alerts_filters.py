9a1fffd2821c7316f7a83acbcd62484f6a6a394a
Elise17 <elise17web@gmail.com>
2025-10-17 20:41:25 +0200

    Added test_get_alert_filter_should_return_404_when_user_has_not_created_alert_filter

--- tests/tests_rest_alerts_filters.py
+++ tests/tests_rest_alerts_filters.py
@@ -227,3 +227,36 @@ class TestsRestAlertsFilters(TestCase):
     def test_get_alert_filter_should_return_404_when_alert_filter_not_found(self):
         response = self._subject.get(f'/api/v2/alerts-filters/{_IDENTIFIER_FOR_NONEXISTENT_OBJECT}')
         self.assertEqual(404, response.status_code)
+
+    def test_get_alert_filter_should_return_404_when_user_has_not_created_alert_filter(self):
+        user = self._subject.create_dummy_user()
+        body = {
+            'filter_is_private': 'true',
+            'filter_type': 'alerts',
+            'filter_name': 'filter_name',
+            'filter_description': 'filter description',
+            'filter_data' : {
+                'alert_tilte': 'filter name',
+                'alert_description': '',
+                'alert_source': '',
+                'alert_tags': '',
+                'alert_severity_id': '',
+                'alert_start_date': '',
+                'source_start_date': '',
+                'source_end_date': '',
+                'creation_end_date': '',
+                'creation_start_date': '',
+                'alert_assets': '',
+                'alert_iocs': '',
+                'alert_ids': '',
+                'source_reference': '',
+                'case_id': '',
+                'custom_conditions': '',
+
+            }
+        }
+
+        response = self._subject.create('/api/v2/alerts-filters', body).json()
+        identifier = response['filter_id']
+        response = user.get(f'/api/v2/alerts-filters/{identifier}')
+        self.assertEqual(404, response.status_code)
