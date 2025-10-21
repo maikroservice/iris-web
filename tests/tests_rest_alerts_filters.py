930e94c6e26d3910d6399c51f8c4b1d5f99eed51
Elise17 <elise17web@gmail.com>
2025-10-21 13:50:59 +0200

    Added test_update_alert_filter_should_return_filter_description

--- tests/tests_rest_alerts_filters.py
+++ tests/tests_rest_alerts_filters.py
@@ -331,3 +331,39 @@ class TestsRestAlertsFilters(TestCase):
         }
         response = self._subject.update(f'/api/v2/alerts-filters/{identifier}', body).json()
         self.assertEqual(filter_name, response['filter_name'])
+
+    def test_update_alert_filter_should_return_filter_description(self):
+        filter_description = 'new filter description'
+        body = {
+            'filter_is_private': 'true',
+            'filter_type': 'alerts',
+            'filter_name': 'old name',
+            'filter_description': 'filter description',
+            'filter_data' : {
+                'alert_tilte': 'filter name',
+                'alert_description': '',
+                'alert_source': '',
+                'alert_tags': '',
+                'alert_severity_id': '',
+                'alert_start_date': '',
+                'source_start_date': '',
+                'source_end_date': '',
+                'creation_end_date': '',
+                'creation_start_date': '',
+                'alert_assets': '',
+                'alert_iocs': '',
+                'alert_ids': '',
+                'source_reference': '',
+                'case_id': '',
+                'custom_conditions': '',
+
+            }
+        }
+
+        response = self._subject.create('/api/v2/alerts-filters', body).json()
+        identifier = response['filter_id']
+        body = {
+            'filter_description': filter_description,
+        }
+        response = self._subject.update(f'/api/v2/alerts-filters/{identifier}', body).json()
+        self.assertEqual(filter_description, response['filter_description'])
