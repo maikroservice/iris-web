58900f8fa92646cfa7a1a36e4680b23dad7b67ca
c8y3 <25362953+c8y3@users.noreply.github.com>
2025-11-21 09:11:19 +0100

    Added test for field total of GET /api/v2/global-tasks

--- tests/tests_rest_global_tasks.py
+++ tests/tests_rest_global_tasks.py
@@ -91,3 +91,9 @@ class TestsRestGlobalTasks(TestCase):
     def test_search_global_tasks_should_return_200(self):
         response = self._subject.get('/api/v2/global-tasks')
         self.assertEqual(200, response.status_code)
+
+    def test_search_global_tasks_should_return_total(self):
+        body = {'task_title': 'dummy title', 'task_status_id': 1, 'task_assignee_id': ADMINISTRATOR_USER_IDENTIFIER}
+        response = self._subject.create('/api/v2/global-tasks', body).json()
+        response = self._subject.get('/api/v2/global-tasks').json()
+        self.assertEqual(1, response['total'])
